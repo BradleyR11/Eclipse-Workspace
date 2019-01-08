1d9888984a731fe5e07a09465096b9beb7bdf4b1
Bradleyr1 <Bradleyr1@091DS-1182-16.student.cssd.ab.ca>
2019-01-08 13:30:16 -0700

    OOP Final Project

---
+++ tutoriaPackage/src/Invoice_printer/TestCustomer1.java
@@ -0,0 +1,29 @@
+package Invoice_printer;
+public class TestCustomer1 {
+public static void main(String[] args) {
+		
+		Customer c0 = new Customer();
+		System.out.println(c0.toString());
+		Customer c1 = new Customer("Home","Bobby",10.50);
+		System.out.println(c1.toString());
+		c1.addFunds(500.0);
+		System.out.println(c1.toString());
+		System.out.println("");
+		
+		Product p0 = new Product();
+		System.out.println(p0.toString());
+		Product p1 = new Product("Flame Thrower", 160.75);
+		System.out.println(p1.toString());
+		System.out.println("");
+		
+		LineItem l0 = new LineItem();
+		System.out.println(l0.toString());
+		LineItem l1 = new LineItem(10 ,new Product("Paper",0.50));
+		System.out.println(l1.toString());
+		System.out.println("");
+		
+		
+
+	}
+
+}
